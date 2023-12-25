adc4d766d964621637cc2af1bc7a353b1d2e4a7b
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-12-25 15:00:59 +0400

    add 'created_at' column in database

--- server/database.sql
+++ server/database.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE perntodo;
-
-CREATE TABLE todo(
+CREATE TABLE todo (
     todo_id SERIAL PRIMARY KEY,
     title VARCHAR(50),
     description VARCHAR(255),
-    status VARCHAR (50)
+    status VARCHAR(50),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
--- server/server.js
+++ server/server.js
@@ -28,7 +28,9 @@ app.post('/todos', async (req, res) => {
 
 app.get('/todos', async (req, res) => {
   try {
-    const allTodos = await pool.query('SELECT * FROM todo');
+    const allTodos = await pool.query(
+      'SELECT * FROM todo ORDER BY created_at DESC'
+    );
     res.json(allTodos.rows);
   } catch (err) {
     console.error(err.message);
